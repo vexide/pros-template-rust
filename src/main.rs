1f45890c9e5722b9e6dbd92641bd6a7adc1590f4
doinkythederp <doinkythederp@icloud.com>
2023-12-15 22:25:01 -0800

    feat: improve robot code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,38 @@
 #![no_main]
 #![no_std]
 
+use core::time::Duration;
+
 use pros::prelude::*;
 
-fn main() {
-    println!("Hello, world!");
+/// Store robot state that will be used throughout the program.
+struct VexRobot {
+    controller: Controller,
 }
+
+impl VexRobot {
+    /// Create subsystems and set up anything that will be used throughout the program.
+    /// This function is run by PROS as soon as the robot program is selected.
+    pub fn new() -> Self {
+        Self {
+            controller: Controller::Master,
+        }
+    }
+}
+
+impl Robot for VexRobot {
+    /// Runs when the robot is enabled.
+    fn opcontrol(&mut self) -> pros::Result {
+        loop {
+            let input = self.controller.state();
+            let Joysticks { left, right } = input.joysticks;
+
+            println!("Speed %: {:?}, Turn %: {:?}", left.y, right.x);
+
+            sleep(Duration::from_millis(20));
+        }
+    }
+}
+
+// Register the robot with PROS so that its methods will be called.
+robot!(VexRobot, VexRobot::new());
